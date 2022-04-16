f313086e73d77765e37a112e17c6d2f0d0281873
Vladimir Ponuzhaev <vep99@mail.ru>
2022-04-16 19:52:35 +0300

    added a description of the task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 
+// Например: 2, 3, 7 -> 7; 44 5 78 -> 78; 22 3 9 -> 22.
+
 Console.WriteLine("Hello, World!");
