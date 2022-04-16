fdcd8d14abe35e445af007390dc5a7a41ded3b4a
Vladimir Ponuzhaev <vep99@mail.ru>
2022-04-16 22:17:41 +0300

    started typing 4 code

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Например: 5 -> 2, 4; 8 -> 2, 4, 6, 8.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int count = 0;
+
+while (number % 2 == 0)
+{
+    DataMisalignedException
+    count = count + 1;
+}
+
+
+
