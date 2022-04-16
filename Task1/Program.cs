31cd9a65c4287593132ce305bcf4dd88b193ad5e
Vladimir Ponuzhaev <vep99@mail.ru>
2022-04-16 20:07:13 +0300

    correted code 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,25 +1,28 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // Например: a = 5, b = 7 -> max = 7, min = 5; a = 2, b = 10 -> max = 10, min = =2; a = -9, b = -3 -> max = -3, min = -9
 
-Console.Clear();
+using System;
+using static System.Console;
 
-Console.WriteLine("Введите первое число: ");
-int numberA = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите второе число: ");
-int numberB = int.Parse(Console.ReadLine()!);
+Clear();
+
+WriteLine("Введите первое число: ");
+int numberA = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int numberB = int.Parse(ReadLine()!);
 
 if (numberA > numberB)
 {
-    Console.WriteLine($"Большее число = {numberA}");
-    Console.WriteLine($"Меньшее число = {numberB}");
+    WriteLine($"Большее число = {numberA}");
+    WriteLine($"Меньшее число = {numberB}");
 }
 else
 {
-    Console.WriteLine($"Большее число = {numberB}");
-    Console.WriteLine($"Меньшее число = {numberA}");
+    WriteLine($"Большее число = {numberB}");
+    WriteLine($"Меньшее число = {numberA}");
 }
 
 if (numberA == numberB)
 {
-    Console.WriteLine("Числа равны");
+    WriteLine("Числа равны");
 }
