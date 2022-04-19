ece68596378c44c5f5b0c20c342e3f245d7a97d7
Vladimir Ponuzhaev <vep99@mail.ru>
2022-04-19 22:12:07 +0300

    added a second version of code 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -6,6 +6,21 @@ using static System.Console;
 
 Clear();
 
+// 1-й способ
+// Write("Введите любое число: ");
+// int AnyNumber = int.Parse(ReadLine()!);
+
+// if (AnyNumber > 99)
+// {
+//     while (AnyNumber > 999)
+//         AnyNumber /= 10;
+//     AnyNumber %= 10;
+//     Console.WriteLine($"В введённом числе третья цифра: {AnyNumber}");
+// }
+// else
+//     Console.WriteLine($"В введённом числе: {AnyNumber}, нет третьей цифры!");
+
+// 2-й способ
 Write("Введите любое число: ");
 int AnyNumber = int.Parse(ReadLine()!);
 
@@ -13,7 +28,7 @@ string ThirdDigit = AnyNumber.ToString();
 
 if (AnyNumber >= 100)
 {
-    WriteLine(ThirdDigit[2]);
+    WriteLine($"В введённом числе третья цифра: {ThirdDigit[2]}");
 }
 else
 {
