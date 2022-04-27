9464bec9fd4aa2215a948d2479c831c92d497054
Vladimir Ponuzhaev <vep99@mail.ru>
2022-04-27 13:18:31 +0300

    typed code 13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Например: 452 -> 11, 82 -> 10, 9012 -> 12.
+
+using System;
+using static System.Console;
+using static System.Convert;
+
+Write("Введите любое число: ");
+int inputNumber = ToInt32(ReadLine());
+WriteLine($"Сумма цифр числа {inputNumber}: {SumDigits(inputNumber)}");
+
+int SumDigits(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = number + result % 10;
+        number = number / 10;
+    }
+    return result;
+}
