e5c149beb3c7b9f66a939b092d103f64f8fc0a44
Vladimir Ponuzhaev <vep99@mail.ru>
2022-05-01 01:05:14 +0300

    typed code 18

---
+++ Task18/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 18: Пользователь вводит с клавиатуры N чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Например: 0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223 -> 4.
+
+using System;
+using static System.Console;
+using static System.Convert;
+
+Clear();
+
+Write("Введите любые числа через пробел: ");
+double[] N = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), ToDouble);
+double result = N.Count(i => i > 0);
+WriteLine();
+WriteLine($"Количество чисел больше 0 –> {result}");
+WriteLine();
