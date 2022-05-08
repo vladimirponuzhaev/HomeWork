3f8437646cf18377edf5b7d55f3cd1fcbd25b629
Vladimir Ponuzhaev <vep99@mail.ru>
2022-05-08 21:02:39 +0300

    minor changes added

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,16 +1,23 @@
-﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+﻿// Задача 19: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // Например: k1 = 5, b1 = 2; k2 = 9, b2 = 4 -> (-0,5; -0,5).
 
-Console.Write("Введите k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
+using System;
+using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+Clear();
+
+Write("Введите k1: ");
+double k1 = ToDouble(ReadLine());
+Write("Введите b1: ");
+double b1 = ToDouble(ReadLine());
+Write("Введите k2: ");
+double k2 = ToDouble(ReadLine());
+Write("Введите b2: ");
+double b2 = ToDouble(ReadLine());
 
 double x = (b2 - b1) / (k1 - k2);
 double y = k1 * x + b1;
 
-Console.WriteLine($"Пересечение в точке: ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+WriteLine($"Пересечение в точке: ({Round(x, 2)};{Round(y, 2)})");
