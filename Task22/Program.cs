37ff7771e55ccf4a1c386ade4473c3df6270231f
Vladimir Ponuzhaev <vep99@mail.ru>
2022-05-15 21:00:12 +0300

    typed code 22

---
+++ Task22/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 22: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив: 1 4 7 2
+//                         5 9 2 3
+//                         8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+using System;
+using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+Clear();
+
+int row = 3;
+int col = 4;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+double[] arithmeticMean = GetArithmeticMean(matrix);
+PrintArray(arithmeticMean);
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"  {array[i, j]}  ");
+        }
+        WriteLine();
+    }
+}
+
+double[] GetArithmeticMean(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(0);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    WriteLine("--------------------");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($" {Round(array[i], 1)}  ");
+    }
+    WriteLine();
+}
