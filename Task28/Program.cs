37bcce04626ed5261b52345e8dd7888d9b77ab69
Vladimir Ponuzhaev <vep99@mail.ru>
2022-05-20 22:06:31 +0300

    typed code 28

---
+++ Task28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа по убыванию в промежутке от M до N.
+// Например: M = 1, N = 5 -> "5, 4, 3, 2, 1"; M = 4, N = 8 -> "8, 7, 6, 5, 4".
+
+using System;
+using static System.Console;
+
+Clear();
+
+void PrintNumbers(int FirstNumber, int LastNumber)
+{
+    if (LastNumber < FirstNumber) return;
+    PrintNumbers(FirstNumber + 1, LastNumber);
+    Write(FirstNumber + " ");
+}
+PrintNumbers(1, 5);
+WriteLine();
+
+int M = 4;
+int N = 8;
+
+string result = GetNumbers(M, N);
+WriteLine(result);
+
+string GetNumbers(int FirstNumber, int LastNumber)
+{
+    if (LastNumber == FirstNumber) return LastNumber.ToString();
+    return (LastNumber + " " + GetNumbers(FirstNumber, LastNumber - 1));
+}
